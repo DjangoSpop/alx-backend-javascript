1e8f4cecf7e784cfc60fae69f066358a18e4b0aa
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-10 15:33:15 +0200

    Refactor NeighborhoodsList class to use arrow function in 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,11 @@
-export default class NeighborhoodsList {
-    constructor() {
+export default function NeighborhoodsList (){
+    
         this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-    }
+    
 
     addNeighborhood = (newNeighborhood) => {
         this.sanFranciscoNeighborhoods.push(newNeighborhood);
         return this.sanFranciscoNeighborhoods;
-    };
-}
+    }
+};
+
