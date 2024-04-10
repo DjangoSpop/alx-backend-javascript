640a47bd4500ffa25c05ce6498fcc91a16b0aa92
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-10 09:30:50 +0200

    Update package.json with @babel/preset-env dependency

---
+++ 0x00-ES6_basic/0-constants.test.js
@@ -0,0 +1,7 @@
+// 0-constants.test.js
+const constants = require('./0-constants.js');
+
+test('Test description', () => {
+  // Your test goes here. For example:
+  expect(constants.someConstant).toBe(expectedValue);
+});
