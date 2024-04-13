b40f7c2e4becab3c50a18a47df97c52e1e7cc26e
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-13 17:40:20 +0200

    Refactor handleResponseFromAPI function in 0x01-ES6_promise/2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  if (promise instanceof Promise) {
-    promise.then(() => {
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    }).catch(() => {
-      console.error('Failed to get a response from the API');
-      return new Error();
-    });
-  }
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
 }
