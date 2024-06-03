2fbfdb92703564de7d24d20f2c766b894b507bd1
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-06-04 02:32:51 +0300

    we can

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+import displayMessage from './0-console';
+
+
+
+displayMessage('Welcome to Holberton School, what is your name?\n');
+const name = displayMessage('please enter your name');
+console.log(`Your name is, ${name}`);
