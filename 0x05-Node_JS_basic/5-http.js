60ccb8b8c1cdfa38093d9a8c1c99a8c808b845b4
BehoSpop <behodjango@gmail.com>
2024-06-04 23:57:36 +0200

    Refactor countStudents function to improve readability and error handling

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const fs = require('fs');
-
 const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
+module.exports = app;
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
 
